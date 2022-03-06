1724d1a1af46b4e484daa2bbea9848545d6fa795
Jeremiah Holt <jholt0147@gmail.com>
2022-03-05 22:58:03 -0500

    adding features

---
+++ CMakeFiles/icewm_prefs.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/icewm_prefs_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/icewm_prefs_autogen.dir/ParseCache.txt"
+  "icewm_prefs_autogen"
+  "CMakeFiles/icewm_prefs.dir/icewm_prefs.cpp.o"
+  "CMakeFiles/icewm_prefs.dir/icewm_prefs.cpp.o.d"
+  "CMakeFiles/icewm_prefs.dir/icewm_prefs_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/icewm_prefs.dir/icewm_prefs_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/icewm_prefs.dir/main.cpp.o"
+  "CMakeFiles/icewm_prefs.dir/main.cpp.o.d"
+  "icewm_prefs"
+  "icewm_prefs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/icewm_prefs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/icewm_prefs_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/icewm_prefs_autogen"
+  "icewm_prefs_autogen/include/ui_icewm_prefs.h"
+  "icewm_prefs_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/icewm_prefs_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
